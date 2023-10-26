a894e363b7676bf61fd796ed3817bafac8450ad0
Adam Šrenkel <adam.srenkel@student.tuke.sk>
2023-10-26 20:53:12 +0200

    try call backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 import './App.css'
 
+const URL = 'https://pdv-zp-backend.azurewebsites.net/Weather/GetWeatherByCoordinates';
+const data = {
+    lat: 48.716385,
+    lon: 21.261074,
+};
+
+
+async function x() {
+    const d = await fetch(URL, {
+        method: 'POST',
+        body: data
+    }).then((res) => {
+        if (res.ok) {
+            return res.json();
+        } else {
+            console.log('Pardon errorek sa udial');
+        }
+    })
+
+    console.log(d);
+}
+
 function App() {
-  return (
-    <>
-      <h1>
-        Pustime deti von?
-      </h1>
-    </>
-  )
+    return (
+        <>
+            <h1>
+                Pustime deti von?
+            </h1>
+            <button onClick={x}>
+                Fetch data
+            </button>
+        </>
+    )
 }
 
 export default App
