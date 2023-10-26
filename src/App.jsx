12d5ee577442a22ee4cdccca33d423aef9700397
Adam Šrenkel <adam.srenkel@student.tuke.sk>
2023-10-26 21:09:08 +0200

    try axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import './App.css'
 
 const URL = 'https://pdv-zp-backend.azurewebsites.net/Weather/GetWeatherByCoordinates';
@@ -6,20 +7,12 @@ const data = {
     lon: 21.261074,
 };
 
-
-async function x() {
-    const d = await fetch(URL, {
-        method: 'POST',
-        body: data
-    }).then((res) => {
-        if (res.ok) {
-            return res.json();
-        } else {
-            console.log('Pardon errorek sa udial');
-        }
-    })
-
-    console.log(d);
+async function fetchData() {
+    await axios.post(URL, data).then((res) => {
+        console.log(res);
+    }).catch((err) => {
+        console.log(err);
+    });
 }
 
 function App() {
@@ -28,7 +21,7 @@ function App() {
             <h1>
                 Pustime deti von?
             </h1>
-            <button onClick={x}>
+            <button onClick={fetchData}>
                 Fetch data
             </button>
         </>
