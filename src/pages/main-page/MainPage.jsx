e0971d9dd432eb8d6ff0aee88821674e7ba4a144
samuels165 <samuel.slebodnik@student.tuke.sk>
2023-11-28 20:18:38 +0100

    main scren human

--- src/pages/main-page/MainPage.jsx
+++ src/pages/main-page/MainPage.jsx
@@ -3,18 +3,19 @@ import './main-page.css'
 import {Header} from "../../components/header/Header.jsx";
 import {Navigation} from "../../components/navigation/Navigation.jsx";
 import {WeatherPanel} from "../../components/weather-info/WeatherPanel.jsx";
+import DressupPanel from "../../components/dressup-info/DressupPanel.jsx";
 
 export function MainPage() {
     return (
-        <>
-            <Header />
-            <Navigation />
-            <div className='mainPanels'>
-                <WeatherPanel />
-                <div className='filler'>
-                    POZDRAVUJEM 2
-                </div>
-            </div>
-        </>
+      <>
+        <Header />
+        <Navigation />
+        <div className="mainPanels">
+          <WeatherPanel />
+          <div className="dressupPanel">
+            <DressupPanel />
+          </div>
+        </div>
+      </>
     );
 }
